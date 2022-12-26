d6544379da3fbc1a6eecf9b281786cf4aa3d4e24
eayuwna <121429604+eayuwna@users.noreply.github.com>
2022-12-26 13:47:08 +0000

    Added game

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def on_button_pressed_a():
-    global Stuff, Age
-    if Age == 0:
+    global Stuff, Times_Fed, Age
+    if Age <= 2 and Times_Fed >= 10:
         Stuff = 0
         music.play_melody("D E F G - - G - ", 388)
         basic.show_leds("""
@@ -43,14 +43,72 @@ def on_button_pressed_a():
                         . # # # .
         """)
         music.play_melody("C5 B A B A B C5 C5 ", 388)
-        Age = 1
+        Times_Fed = 0
+        Age += 1
         Stuff = 1
 input.on_button_pressed(Button.A, on_button_pressed_a)
+
+def on_button_pressed_ab():
+    global Stuff, Direction2
+    if Age == 2:
+        Stuff = 0
+        basic.show_string("Game!")
+        for index in range(4):
+            basic.show_leds("""
+                . # # # .
+                                # . . . #
+                                # . # # #
+                                # . # # #
+                                . # # # .
+            """)
+            basic.show_leds("""
+                . # # # .
+                                # . . . #
+                                # # # . #
+                                # # # . #
+                                . # # # .
+            """)
+        while not (input.button_is_pressed(Button.A)) or input.button_is_pressed(Button.B):
+            basic.show_leds("""
+                . # # # .
+                                # . . . #
+                                # . # . #
+                                # . . . #
+                                . # # # .
+            """)
+            basic.show_leds("""
+                . # # # .
+                                # . . . #
+                                # . . . #
+                                # . . . #
+                                . # # # .
+            """)
+        if input.button_is_pressed(Button.A):
+            Direction2 = 0
+        else:
+            Direction2 = 1
+        if Math.random_boolean():
+            basic.show_leds("""
+                . # # # .
+                                # . . . #
+                                # . # # #
+                                # . # # #
+                                . # # # .
+            """)
+        else:
+            basic.show_leds("""
+                . # # # .
+                                # . . . #
+                                # # # . #
+                                # # # . #
+                                . # # # .
+            """)
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
 def on_button_pressed_b():
     global Stuff
     Stuff = 0
-    if Age == 1:
+    if Age <= 2:
         basic.show_string("Hungry:")
         music.play_tone(262, music.beat(BeatFraction.WHOLE))
         if Hungry < 10:
@@ -106,8 +164,8 @@ def on_button_pressed_b():
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
 def on_logo_pressed():
-    global Stuff, Hungry
-    if Age == 1:
+    global Stuff, Hungry, Times_Fed
+    if Age <= 2:
         Stuff = 0
         basic.show_string("Yummy!")
         music.play_melody("B - - B A E - - ", 388)
@@ -134,18 +192,21 @@ def on_logo_pressed():
         """)
         Hungry = 0
         Stuff = 1
+        Times_Fed += 1
 input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_pressed)
 
-b_born = 0
+Direction2 = 0
+Times_Fed = 0
 Stuff = 0
 Hungry = 0
 Age = 0
 Age = 0
 Hungry = 0
 Stuff = 1
+Times_Fed = 0
 
 def on_forever():
-    global Hungry, b_born
+    global Hungry
     if Stuff == 1:
         if Age == 0:
             basic.show_leds("""
@@ -162,7 +223,41 @@ def on_forever():
                                 # . # . #
                                 . # # # .
             """)
-        if b_born == 1:
+        if Age == 1:
+            if Hungry < 70:
+                basic.show_leds("""
+                    . . # . .
+                                        . . # . .
+                                        # # # # #
+                                        . . # . .
+                                        . . # . .
+                """)
+                basic.show_leds("""
+                    . . . . .
+                                        . . # . .
+                                        . # # # .
+                                        . . # . .
+                                        . . . . .
+                """)
+                Hungry += 1
+            if Hungry >= 70:
+                basic.show_leds("""
+                    . # . # .
+                                        . # . # .
+                                        . # # # .
+                                        . # # # .
+                                        . # # # .
+                """)
+                basic.show_leds("""
+                    . # . # .
+                                        . # . # .
+                                        . # # # .
+                                        . # . # .
+                                        . # . # .
+                """)
+                music.play_tone(262, music.beat(BeatFraction.WHOLE))
+                Hungry += 1
+        if Age == 2:
             if Hungry < 70:
                 basic.show_leds("""
                     . # # # .
@@ -180,7 +275,6 @@ def on_forever():
                 """)
                 Hungry += 1
             if Hungry >= 70:
-                b_born = 1
                 basic.show_leds("""
                     . # # # .
                                         # . . . #
--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 input.onButtonPressed(Button.A, function () {
-    if (Age <= 1 && Times_Fed >= 10) {
+    if (Age <= 2 && Times_Fed >= 10) {
         Stuff = 0
         music.playMelody("D E F G - - G - ", 388)
         basic.showLeds(`
@@ -47,9 +47,81 @@ input.onButtonPressed(Button.A, function () {
         Stuff = 1
     }
 })
+input.onButtonPressed(Button.AB, function () {
+    if (Age == 2) {
+        Stuff = 0
+        basic.showString("Game!")
+        for (let index = 0; index < 4; index++) {
+            basic.showLeds(`
+                . # # # .
+                # . . . #
+                # . # # #
+                # . # # #
+                . # # # .
+                `)
+            basic.showLeds(`
+                . # # # .
+                # . . . #
+                # # # . #
+                # # # . #
+                . # # # .
+                `)
+        }
+        while (!(input.buttonIsPressed(Button.A)) || input.buttonIsPressed(Button.B)) {
+            basic.showLeds(`
+                . # # # .
+                # . . . #
+                # . # . #
+                # . . . #
+                . # # # .
+                `)
+            basic.showLeds(`
+                . # # # .
+                # . . . #
+                # . . . #
+                # . . . #
+                . # # # .
+                `)
+        }
+        if (input.buttonIsPressed(Button.A)) {
+            Direction2 = 0
+        } else {
+            Direction2 = 1
+        }
+        Choice = randint(0, 1)
+        if (Choice == 1) {
+            basic.showLeds(`
+                . # # # .
+                # . . . #
+                . . # # #
+                # . # # #
+                . # # # .
+                `)
+            if (1 == Direction2) {
+                basic.showString("Win")
+            } else {
+                basic.showString("Lose")
+            }
+        } else {
+            basic.showLeds(`
+                . # # # .
+                # . . . #
+                # # # . #
+                # # # . #
+                . # # # .
+                `)
+            if (0 == Direction2) {
+                basic.showString("Win")
+            } else {
+                basic.showString("Lose")
+            }
+        }
+    }
+    Stuff = 1
+})
 input.onButtonPressed(Button.B, function () {
     Stuff = 0
-    if (Age <= 1) {
+    if (Age <= 2) {
         basic.showString("Hungry:")
         music.playTone(262, music.beat(BeatFraction.Whole))
         if (Hungry < 10) {
@@ -106,12 +178,11 @@ input.onButtonPressed(Button.B, function () {
                 . # # # .
                 `)
         }
-        basic.pause(4000)
     }
     Stuff = 1
 })
 input.onLogoEvent(TouchButtonEvent.Pressed, function () {
-    if (Age <= 1) {
+    if (Age <= 2) {
         Stuff = 0
         basic.showString("Yummy!")
         music.playMelody("B - - B A E - - ", 388)
@@ -141,6 +212,8 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function () {
         Times_Fed += 1
     }
 })
+let Choice = 0
+let Direction2 = 0
 let Times_Fed = 0
 let Stuff = 0
 let Hungry = 0
